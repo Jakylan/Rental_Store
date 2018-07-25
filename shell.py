65f3290da07b7c88fe0e6eb5926d6bfa26196b87
Jakylan <jstandifer@basecampcodingacademy.org>
2018-07-25 14:21:12 -0500

    i have added prices now and now i have to get it to where it take it out of the stock and return it.

--- shell.py
+++ shell.py
@@ -1,6 +1,6 @@
 def parse_inventory_item(string):
-    rental, in_stock, = string.split(',')
-    return [rental, int(in_stock)]
+    rental, in_stock, price = string.split(',')
+    return [rental, int(in_stock), int(price)]
 
 
 def load_inventory():
@@ -14,6 +14,7 @@ def load_inventory():
             inventory[d[0]] = {
                 'Rental': d[0],
                 'In-Stock': d[1],
+                'Price': d[2],
             }
 
     return inventory
@@ -30,6 +31,7 @@ def choose_console(inventory):
         print(item)
         print('   Rental', console['Rental'])
         print('   In-Stock', console['In-Stock'])
+        print('   Price', console['Price'])
     while True:
         item = input('\nWhich console would you like?\n')
         if item in inventory:
@@ -45,52 +47,25 @@ def choose_console(inventory):
 
 def main():
     inventory = load_inventory()
-    print('\Hello welcome to Base Camp Rentals!\n')
+    print('\nHello welcome to Base Camp Rentals!\n')
     response = input('\nAre you a customer or a employee?\n')
-
-    if response == 'customer':
-        print('\nGreat!')
-        print('''
-        1 - Nintendo Switch
-        2 - Xbox One X
-        3 - PlayStation 4 Pro 1TB
-        4 - Nintendo Wii U
-        5 - Xbox 360 E Console
-        6 - PlayStation 3
-        7 - Super NES Classic
-        8 - Nintendo 3DS XL
-        ''')
-        response = input('\nWould you like to rent a console?\n')
-        if response == 'yes':
-            choose_console(inventory)
-            if response == '1':
-                print('\nYou have chosen the Nintendo Switch.\n')
-            elif response == '2':
-                print('\nYou have chosen the Xbox One X.\n')
-            elif response == '3':
-                print('\nYou have chosen the PlayStaion 4 Pro 1TB.\n')
-            elif response == '4':
-                print('\nYou have chosen the Nintendo Wii U.\n')
-            elif response == '5':
-                print('\nYou have chosen the Xbox 360 E Console.\n')
-            elif response == '6':
-                print('\nYou have chosen the PlayStation 3.\n')
-            elif response == '7':
-                print('\nYou have chosen the Super NES Classic.\n')
-            elif response == '8':
-                print('\nYou have chosen the Nintendo 3DS XL.\n')
-
-    elif response == 'employee':
-        print('\nHi, employee!\n')
-        response = input('\nWould you like to view the inventory?\n')
-        if response == 'yes':
-            print(inventory)
-        elif response == 'no':
+    while True:
+        if response == 'customer':
+            print('\nGreat!')
+            response = input('\nWould you like to rent a console?\n')
+            if response == 'yes':
+                choose_console(inventory)
+        elif response == 'employee':
+            print('\nHi, employee!\n')
+            response = input('\nWould you like to view the inventory?\n')
+            if response == 'yes':
+                print(inventory)
+            elif response == 'no':
+                print('\nHave a great day!\n')
+                break
+        elif response == 'quit':
             print('\nHave a great day!\n')
-            exit()
-    elif response == 'quit':
-        print('\nHave a great day!\n')
-        exit()
+            break
 
 
 if __name__ == '__main__':
