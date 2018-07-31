242815b66b3ed25559635aab24aabc90a0e68de6
Jakylan <jstandifer@basecampcodingacademy.org>
2018-07-31 13:34:53 -0500

    my core.py

---
+++ core.py
@@ -0,0 +1,8 @@
+def get_game_price():
+    if item == 'Xbox One X':
+        price = 175
+    elif item == 'PlayStation 4 Pro 1TB':
+        price = 150
+    elif item == 'Super NES Classic':
+        price = 75
+    return price
