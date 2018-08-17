3f8203c5fb3220a109aca5c2c3126b52f9b06c0a
Jakylan <jstandifer@basecampcodingacademy.org>
2018-08-17 13:26:09 -0500

    disk.py

--- disk.py
+++ disk.py
@@ -2,26 +2,32 @@ from datetime import datetime
 import core
 
 
-def read_file():
-    with open('inventory.txt', 'r') as file:
+def read_file(file):
+    with open(file, 'r') as file:
+        file.readline()
         data = file.readlines()
-    inventory = {}
-    for item_info in data:
-        item = item_info.split(',')
-        item_name = item[0].strip()
-        in_stock = int(item[1]).strip()
-        price = int(item[2]).strip()
-        inventory[item] = {
-            'rental': item,
-            'in-stock': in_stock,
-            'price': price,
-            'replacement': replacement
-        }
-    return inventory
+    return data
 
 
-def write_file(time, item, in_stock, price, replacement):
-    text = '\n{}, {}, {}'.format(time, in_stock, price, replacement)
+def file_contents(file):
+    with open(file) as file:
+        contents = file.read()
+    return contents
+
+
+def write_file(item, in_stock, price, replacement, action):
+    text = '\n{}, {}, {}, {}, {}'.format(datetime.now(), in_stock, price,
+                                         replacement, action)
 
     with open('history.txt', 'a') as file:
         data = file.write(text)
+
+
+def write_revenue(string, file):
+    with open(file, 'w') as file:
+        file.write(string)
+
+
+def write_inventory(inventory, file):
+    with open(file, 'w') as file:
+        file.write(inventory)
